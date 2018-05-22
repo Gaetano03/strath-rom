a4ae46da786ed9ce3ca45d0d831b1df1d56ce037
Gaetano <gaetano.pascarella@strath.ac.uk>
2018-05-22 14:08:08 +0100

    MainVortDetectionUpdate

--- examples/Main_Vortex_detection.cpp
+++ examples/Main_Vortex_detection.cpp
@@ -6,10 +6,10 @@
 int main( int argc, char *argv[] ){
 
 printf("Program name %s\n", argv[0]);
-if( argc == 5 ) {
-    printf("Number of snaps: %s, Delta snap: %s, First snap: %s, directory: %s\n", argv[1], argv[2], argv[3], argv[4]);
+if( argc == 6 ) {
+    printf("Number of snaps: %s, Delta snap: %s, First snap: %s, Prob dimension: %s, directory: %s\n", argv[1], argv[2], argv[3], argv[4], argv[5]);
 }
-else if( argc > 5 ) {
+else if( argc > 6 ) {
     printf("Too many arguments supplied.\n");
 }
 else {
@@ -19,32 +19,13 @@ else {
 
 cout << "Main start" << endl;
 
-string directory = argv[4];
-string filename = directory + "restart_flow.dat";
-
-unsigned int Nr = Ngrid_points( filename, 22 );
-
-cout << "Number of grid points: " << Nr << endl;
-
-int Nc = 33;
 int Ns = atoi( argv[1] );
 int read_sol_freq = atoi( argv[2] );
 int first_snap = atoi( argv[3] );
 
-
-
-vector<int> col_xyz = { 1, 2, 3 };
-vector<int> col_grads = { 21, 22, 23, 24, 25, 26, 27, 28, 29};
-vector<string> headers_vortex_det(13);
-int N_grads;
-N_grads = col_grads.size();
-VectorXd x(Nr);
-VectorXd y(Nr);
-VectorXd z(Nr);
-
-MatrixXd mat_xyz(Nr, 3);
-// MatrixXd mat_Vortex_Det(Nr, 6);
-
+string cas = argv[4];
+string directory = argv[5];
+string filename = directory + "restart_flow.dat";
 
 string file_in = directory + "gradients/gradients_";
 string file_out = directory + "vortices_criteria/Vortex_Criteria_";
@@ -52,6 +33,7 @@ string file_temp_in, file_temp_out;
 string format_in = ".csv";
 string format_out = ".dat";
 
+vector<string> headers_vortex_det(13);
 headers_vortex_det[0] = "\"PointID\"";
 headers_vortex_det[1] = "\"x\"";
 headers_vortex_det[2] = "\"y\"";
@@ -66,20 +48,67 @@ headers_vortex_det[10] = "\"N_trusdell\"";
 headers_vortex_det[11] = "\"Vort_magnitude\"";
 headers_vortex_det[12] = "\"Rortex\"";
 
+unsigned int Nr;
+int Nc;
+VectorXd x(Nr);
+VectorXd y(Nr);
+VectorXd z(Nr);
+vector<int> col_grads;
 
-clock_t chrono_begin, chrono_end;
-double comp_time;
+if ( cas == "3D"){
 
-chrono_begin = clock();
+    Nr = Ngrid_points( filename, 22 );
+    cout << "Number of grid points: " << Nr << endl;
 
-cout << "Reading coordinates" << endl;
-read_restartDat ( filename, col_xyz, 22, mat_xyz );
-
-x = mat_xyz.col(0);
-y = mat_xyz.col(1);
-z = mat_xyz.col(2);
+    Nc = 33;
+       
 
 
+    vector<int> col_xyz = { 1, 2, 3 };
+    col_grads = { 21, 22, 23, 24, 25, 26, 27, 28, 29};
+    int N_grads;
+    N_grads = col_grads.size();
+
+
+    MatrixXd mat_xyz(Nr, 3);
+    // MatrixXd mat_Vortex_Det(Nr, 6);
+
+    cout << "Reading coordinates" << endl;
+    read_restartDat ( filename, col_xyz, 22, mat_xyz );
+
+    x = mat_xyz.col(0);
+    y = mat_xyz.col(1);
+    z = mat_xyz.col(2);
+
+}
+
+
+if ( cas == "2D"){
+
+    Nr = Ngrid_points( filename, 19 );
+    cout << "Number of grid points: " << Nr << endl;
+
+    Nc = 31;
+       
+
+
+    vector<int> col_xy = { 1, 2 };
+    col_grads = { 19, 20, 22, 23};
+
+    int N_grads;
+    N_grads = col_grads.size();
+
+    MatrixXd mat_xy(Nr, 2);
+    // MatrixXd mat_Vortex_Det(Nr, 6);
+
+
+    cout << "Reading coordinates" << endl;
+    read_restartDat ( filename, col_xy, 19, mat_xy );
+
+    x = mat_xy.col(0);
+    y = mat_xy.col(1);
+
+}
 
 
 for ( int i = first_snap; i <= (first_snap + read_sol_freq*Ns); i += read_sol_freq ){
@@ -97,19 +126,19 @@ for ( int i = first_snap; i <= (first_snap + read_sol_freq*Ns); i += read_sol_fr
 
     cout << "Calculated vortex criteria" << endl;
     
-    write_restart3D(file_temp_out, headers_vortex_det, x, y, z, mat_Vortex_Det);
+    if ( cas == "2D"){
+        write_restart2D(file_temp_out, headers_vortex_det, x, y, mat_Vortex_Det);
+    }
+    else{
+        write_restart3D(file_temp_out, headers_vortex_det, x, y, z, mat_Vortex_Det);
+    }
 
     cout << file_temp_out << endl;
 
 }
 
 
-chrono_end = clock();
-comp_time = ((double)(chrono_end - chrono_begin))/CLOCKS_PER_SEC;
-
-cout << " Computational time :" << comp_time << endl;
-
-cout << "Main end\n" << endl;
+cout << "Main end\n " << endl;
 
 return 0;
 
